c2f34125732ffa05b7ac5fe70602604f0c62ddb1
edmond <edmond.wanyama@student.moringaschool.com>
2023-08-18 13:17:17 +0300

    initial commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+
+   even_num=num_list
+   even_nos = [num for num in even_num if num % 2 == 0]
+   return even_nos
+
+return_evens([0,1, 3, 5, 7, 8, 9])
+
+
 
 def make_exclamation(sentence_list):
-    pass
+
+   sentence_plus_exclamation=sentence_list
+   new_list = [sentence+'!' for sentence in sentence_plus_exclamation]
+   return new_list
+
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
